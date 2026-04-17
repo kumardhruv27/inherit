9ae2ddf3878e384c091cd3717733f49e07f3b243
dhruv <dhruvkumar22504@gmail.com>
2026-04-17 15:19:17 +0530

    inheritence

---
+++ plant/src/Plant.java
@@ -0,0 +1,27 @@
+class plant{
+    String name;
+    void grow(){
+        System.out.println("plant grow");
+    }
+}
+class floweringplant extends plant{
+    void productflowers(){
+        System.out.println("this plant produces flowers");
+    }
+}
+class rose extends floweringplant{
+    void showDetails(){
+        name="rose";
+        System.out.println("plant name :"+name);
+        System.out.println("rose is a flowering plant");
+    }
+}
+
+public class Plant {
+    public static void main(String[] args){
+        rose r = new rose();
+        r.grow();
+        r.productflowers();
+        r.showDetails();
+    }
+}
